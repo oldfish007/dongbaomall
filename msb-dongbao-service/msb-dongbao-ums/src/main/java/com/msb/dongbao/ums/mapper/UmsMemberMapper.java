8c434e00ad3a7d9ddaf93f4b9fc89ad5c3ef0556
zxh <466403678@qq.com>
2022-01-09 15:28:51 +0800

    第一个程序测试mapper

---
+++ msb-dongbao-service/msb-dongbao-ums/src/main/java/com/msb/dongbao/ums/mapper/UmsMemberMapper.java
@@ -0,0 +1,16 @@
+package com.msb.dongbao.ums.mapper;
+
+import com.msb.dongbao.ums.entity.UmsMember;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 后台用户表 Mapper 接口
+ * </p>
+ *
+ * @author oldfish007
+ * @since 2022-01-09
+ */
+public interface UmsMemberMapper extends BaseMapper<UmsMember> {
+
+}
---
+++ msb-dongbao-service/msb-dongbao-ums/src/test/java/com/msb/dongbao/ums/mapper/UserMemberTest.java
@@ -0,0 +1,26 @@
+package com.msb.dongbao.ums.mapper;
+
+import com.msb.dongbao.ums.entity.UmsMember;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = com.msb.dongbao.ums.MsbDongbaoUmsApplication.class)
+public class UserMemberTest {
+
+    @Autowired
+    UmsMemberMapper umsMemberMapper;
+    @Test
+    void testInsert(){
+        UmsMember t = new UmsMember();
+        t.setEmail("466403678@qq.com");
+        t.setUsername("cpf2");
+        t.setStatus(0);
+        t.setPassword("1");
+        t.setNote("note");
+        t.setNickName("nick");
+        umsMemberMapper.insert(t);
+
+
+    }
+}
